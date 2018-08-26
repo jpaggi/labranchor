975a29df8f579673bfff43e0dbec0d38c05e6fe1
jpaggi <jpaggi@stanford.edu>
2018-08-26 15:39:40 -0400

    Submission.

--- preprocessing/run_preprocessing.sh
+++ preprocessing/run_preprocessing.sh
@@ -11,18 +11,18 @@
 # Entries consist of:
 # CHROM (THREE-100) THREE GENE_ID FIVE STRAND
 # The coordinates are transposed if the 3'ss aligns to the minus strand.
-cat ../anno/gencode.v19.annotation.gtf | python bp_intron_bed.py | sort -u -k1,1 -k2,3n > gencode.v19.introns.bpregion.bed
+#cat ../anno/gencode.v19.annotation.gtf | python bp_intron_bed.py | sort -u -k1,1 -k2,3n > gencode.v19.introns.bpregion.bed
 
 # Put Tagart et al annotations into bed format
-cat ../anno/taggart.supptable1.csv | python taggart_to_bed.py | sort -k1,1 -k2,3n > ../anno/taggart.supptable1.bed
+#cat ../anno/taggart.supptable1.csv | python taggart_to_bed.py | sort -k1,1 -k2,3n > ../anno/taggart.supptable1.bed
 
 # Put Signal et al predictions into bed format
-tail -n +2 ../anno/gencode_v19_branchpoints.csv | awk -F , '{print $1"\t"$2"\t"$2+1"\t"$5"\t.\t"$4}' > ../anno/gencode_v19_branchpoints.bed
+#tail -n +2 ../anno/gencode_v19_branchpoints.csv | awk -F , '{print $1"\t"$2"\t"$2+1"\t"$5"\t.\t"$4}' > ../anno/gencode_v19_branchpoints.bed
 
 # Join bp annotations to introns.
-bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/mercer.suppdataS2.bed > introns_to_mercer.tsv
-bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/taggart.supptable1.bed > introns_to_taggart.tsv
-bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/gencode_v19_branchpoints.bed > introns_to_signal.tsv
+#bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/mercer.suppdataS2.bed > introns_to_mercer.tsv
+#bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/taggart.supptable1.bed > introns_to_taggart.tsv
+#bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/gencode_v19_branchpoints.bed > introns_to_signal.tsv
 
 
 ## Prepare conservation scores.
@@ -30,10 +30,10 @@ bedtools intersect -loj -s -a gencode.v19.introns.bpregion.bed -b ../anno/gencod
 ## Comment out if previously run
 
 # Convert conservation scores to bedgraph.
-#./bigWigToBedGraph ../anno/hg19.100way.phastcons.bw ../anno/hg19.100way.phastcons.bedGraph
-#./bigWigToBedGraph ../anno/hg19.100way.phyloP100way.bw ../anno/hg19.100way.phyloP100way.bedGraph
+./bigWigToBedGraph ../anno/hg19.100way.phastcons.bw ../anno/hg19.100way.phastcons.bedGraph
+./bigWigToBedGraph ../anno/hg19.100way.phyloP100way.bw ../anno/hg19.100way.phyloP100way.bedGraph
 
 # Extract only entries we are interested in.
-#bedtools intersect -u -a ../anno/hg19.100way.phyloP100way.bedGraph -b gencode.v19.introns.bpregion.bed > ../anno/hg19.100way.phyloP100way.bp.bed
-#bedtools intersect -u -a ../anno/hg19.100way.phastcons.bedGraph -b gencode.v19.introns.bpregion.bed > ../anno/hg19.100way.phastCons.bp.bed
+bedtools intersect -u -a ../anno/hg19.100way.phyloP100way.bedGraph -b gencode.v19.introns.bpregion.bed > ../anno/hg19.100way.phyloP100way.bp.bed
+bedtools intersect -u -a ../anno/hg19.100way.phastcons.bedGraph -b gencode.v19.introns.bpregion.bed > ../anno/hg19.100way.phastCons.bp.bed
 #bedtools intersect -u -a ../../primates/hg19.phyloP46way.primate.bedGraph -b gencode.v19.introns.bpregion.bed > ../anno/hg19.46way.phylop.primates.bp.bed
